4770493b542e823037527b2e74798efdb2c7eec8
kevnn-m <122247940+kevnn-m@users.noreply.github.com>
2025-01-19 04:00:07 +0000

    Deploying to gh-pages from @ kevnn-m/kevnn-m.github.io@6fe328be59d78841c89bbbcc7b4f32618ea95cae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6B%68%67%75%6F@%75%73%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,7 +506,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=3VM1a_oAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
